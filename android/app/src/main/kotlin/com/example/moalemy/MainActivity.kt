0d08820e1422b3e0284d688f53a9e8b5b9b64726
Yahia Ahmed <yahiapoteto@gmail.com>
2021-06-21 04:34:31 +0200

    try

---
+++ android/app/src/main/kotlin/com/example/moalemy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.moalemy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: mainScreen(),
+    ));
+
+class mainScreen extends StatefulWidget {
+  @override
+  _mainScreenState createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        padding: EdgeInsets.all(100),
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.redAccent[700],
+                child: Text("quiz"),
+                radius: 100,
+              ),
+              Container(
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
